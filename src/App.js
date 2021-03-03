dbfba7abb41cc15e183e5b5500e48854a89341c7
Dorin Botan <dorinbotan@mail.ru>
2021-03-03 08:41:54 +0000

    Added keycloak authentication

--- src/App.js
+++ src/App.js
@@ -5,30 +5,38 @@ import Login from "./views/Login";
 import CashierVideoQueue from "./views/cashier/CashierVideoQueue";
 import ShopperVideoQueue from "./views/shopper/ShopperVideoQueue";
 import ReduxToastr from "react-redux-toastr";
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import keycloak from './keycloak'
 import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
+
+const initOptions = {
+  onLoad: 'login-required'
+}
 
 function App() {
   return (
-    <React.Fragment>
-      <Router basename="/nextgenshop-frontend">
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route path="/login" component={Login} />
-          <Route path="/shop/queue" component={CashierVideoQueue} />
-          <Route path="/cashier/queue" component={ShopperVideoQueue} />
-        </Switch>
-      </Router>
-      <ReduxToastr
-        timeOut={5000}
-        newestOnTop={true}
-        position="top-center"
-        transitionIn="fadeIn"
-        transitionOut="fadeOut"
-        progressBar
-        closeOnToastrClick
-        preventDuplicates
-      />
-    </React.Fragment>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
+      <React.Fragment>
+        <Router basename="/nextgenshop-frontend">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path="/login" component={Login} />
+            <Route path="/shop/queue" component={CashierVideoQueue} />
+            <Route path="/cashier/queue" component={ShopperVideoQueue} />
+          </Switch>
+        </Router>
+        <ReduxToastr
+          timeOut={5000}
+          newestOnTop={true}
+          position="top-center"
+          transitionIn="fadeIn"
+          transitionOut="fadeOut"
+          progressBar
+          closeOnToastrClick
+          preventDuplicates
+        />
+      </React.Fragment>
+    </ReactKeycloakProvider>
   );
 }
 
