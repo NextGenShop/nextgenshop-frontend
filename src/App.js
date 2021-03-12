d5c93b6c30de99906e7369486b9101d55a509500
roxanatugulea <roxana.tugulea@gmail.com>
2021-03-12 18:28:54 +0200

    Added Checkout Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./views/Main";
 import Login from "./views/Login";
+import Checkout from "./views/shopper/CheckoutPage";
 import CashierVideoQueue from "./views/cashier/CashierVideoQueue";
 import ShopperVideoQueue from "./views/shopper/ShopperVideoQueue";
 import ReduxToastr from "react-redux-toastr";
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/shop/queue" component={CashierVideoQueue} />
           <Route path="/cashier/queue" component={ShopperVideoQueue} />
+          <Route path="/checkout" component={Checkout} />
         </Switch>
       </Router>
       <ReduxToastr
