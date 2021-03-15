13c240599099b68cf0ebe5d5f89063c2c5c8e6f0
Brian Min <bymi15@yahoo.com>
2021-03-15 22:27:40 +0000

    feat: connect basket redux store to CheckoutPage

--- src/views/shopper/CheckoutPage.js
+++ src/views/shopper/CheckoutPage.js
@@ -1,32 +1,33 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Paper from "@material-ui/core/Paper";
-import Stepper from "@material-ui/core/Stepper";
-import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import AddressForm from "../../components/checkout/AddressForm";
-import PaymentForm from "../../components/checkout/PaymentForm";
-import Review from "../../components/ReviewOrder";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AddressForm from '../../components/checkout/AddressForm';
+import PaymentForm from '../../components/checkout/PaymentForm';
+import Review from '../../components/ReviewOrder';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    position: "relative",
-    color: "primary",
+    position: 'relative',
+    color: 'primary',
   },
   layout: {
-    width: "auto",
+    width: 'auto',
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
       width: 600,
-      marginLeft: "auto",
-      marginRight: "auto",
+      marginLeft: 'auto',
+      marginRight: 'auto',
     },
   },
   paper: {
@@ -43,8 +44,8 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(3, 0, 5),
   },
   buttons: {
-    display: "flex",
-    justifyContent: "flex-end",
+    display: 'flex',
+    justifyContent: 'flex-end',
   },
   button: {
     marginTop: theme.spacing(3),
@@ -52,7 +53,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const steps = ["Shipping address", "Payment details", "Review your order"];
+const steps = ['Shipping address', 'Payment details', 'Review your order'];
 
 function getStepContent(step) {
   switch (step) {
@@ -63,11 +64,11 @@ function getStepContent(step) {
     case 2:
       return <Review />;
     default:
-      throw new Error("Unknown step");
+      throw new Error('Unknown step');
   }
 }
 
-export default function Checkout() {
+export function Checkout({ basket }) {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
 
@@ -79,13 +80,17 @@ export default function Checkout() {
     setActiveStep(activeStep - 1);
   };
 
+  // TODO:
+  // map through basket.items and render the product name, price, quantity etc.
+  console.log(basket); // REMOVE THIS (for testing purposes)
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="absolute" color="primary" className={classes.appBar}>
+      <AppBar position='absolute' color='primary' className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            <Link to="/login" style={{ textDecoration: "none", color: "#FFF" }}>
+          <Typography variant='h6' color='inherit' noWrap>
+            <Link to='/login' style={{ textDecoration: 'none', color: '#FFF' }}>
               NextGenShop
             </Link>
           </Typography>
@@ -93,7 +98,7 @@ export default function Checkout() {
       </AppBar>
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography component="h1" variant="h4" align="center">
+          <Typography component='h1' variant='h4' align='center'>
             Checkout
           </Typography>
           <Stepper activeStep={activeStep} className={classes.stepper}>
@@ -106,10 +111,10 @@ export default function Checkout() {
           <React.Fragment>
             {activeStep === steps.length ? (
               <React.Fragment>
-                <Typography variant="h5" gutterBottom>
+                <Typography variant='h5' gutterBottom>
                   Thank you for your order.
                 </Typography>
-                <Typography variant="subtitle1">
+                <Typography variant='subtitle1'>
                   Your order number is #2001539. We have emailed your order
                   confirmation, and will send you an update when your order has
                   shipped.
@@ -125,12 +130,12 @@ export default function Checkout() {
                     </Button>
                   )}
                   <Button
-                    variant="contained"
-                    color="primary"
+                    variant='contained'
+                    color='primary'
                     onClick={handleNext}
                     className={classes.button}
                   >
-                    {activeStep === steps.length - 1 ? "Place order" : "Next"}
+                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
                   </Button>
                 </div>
               </React.Fragment>
@@ -141,3 +146,9 @@ export default function Checkout() {
     </React.Fragment>
   );
 }
+
+const mapStateToProps = (state) => ({
+  basket: state.basket,
+});
+
+export default connect(mapStateToProps, null)(Checkout);
